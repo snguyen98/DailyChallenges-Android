04830814b7ba2125e367264f1defdef95422dd60
snguyen98 <snguyen.cs@outlook.com>
2020-05-14 21:00:12 +0100

    Created initial info bar card layout and fragment

---
+++ app/src/main/java/com/firstventuresgroup/dailychallenges/ui/InfoBarFragment.kt
@@ -0,0 +1,17 @@
+package com.firstventuresgroup.dailychallenges.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.firstventuresgroup.dailychallenges.R
+
+class InfoBarFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_info_bar, container, false)
+    }
+}
