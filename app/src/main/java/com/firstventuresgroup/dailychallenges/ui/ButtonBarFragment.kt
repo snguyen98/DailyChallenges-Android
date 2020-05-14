a49d43c1eac6d34f1eee3bc3ccea0854f2617758
snguyen98 <snguyen.cs@outlook.com>
2020-05-14 20:12:22 +0100

    Created initial button bar card layout and fragment

---
+++ app/src/main/java/com/firstventuresgroup/dailychallenges/ui/ButtonBarFragment.kt
@@ -0,0 +1,17 @@
+package com.firstventuresgroup.dailychallenges.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.firstventuresgroup.dailychallenges.R
+
+class ButtonBarFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_button_bar, container, false)
+    }
+}
