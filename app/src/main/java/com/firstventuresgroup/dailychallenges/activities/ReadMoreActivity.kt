c0bf9742150093635d2d385a1ca2bd5e2fc9b7e7
Ned <nedeeshaw@virginmedia.com>
2020-05-27 19:31:08 +0100

    Created new activity for read more section

---
+++ app/src/main/java/com/firstventuresgroup/dailychallenges/activities/ReadMoreActivity.kt
@@ -0,0 +1,13 @@
+package com.firstventuresgroup.dailychallenges.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firstventuresgroup.dailychallenges.R
+
+class ReadMoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read_more)
+    }
+}
