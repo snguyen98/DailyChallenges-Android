d274fc2471f6d1c8284c093ece62d7f53c6201c9
snguyen98 <snguyen.cs@outlook.com>
2020-05-27 20:13:14 +0100

    Removed redundant line

--- app/src/main/java/com/firstventuresgroup/dailychallenges/activities/SplashActivity.kt
+++ app/src/main/java/com/firstventuresgroup/dailychallenges/activities/SplashActivity.kt
@@ -1,14 +1,12 @@
 package com.firstventuresgroup.dailychallenges.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.firstventuresgroup.dailychallenges.R
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
 
         val intent = Intent(this, MainActivity::class.java)
