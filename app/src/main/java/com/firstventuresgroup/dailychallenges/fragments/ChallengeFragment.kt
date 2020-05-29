f7a2494a2495a1d0dcbf93ab7eb910e3eeaa9ed5
snguyen98 <snguyen.cs@outlook.com>
2020-05-29 20:42:31 +0100

    Attached challenge view model to challenge fragment

--- app/src/main/java/com/firstventuresgroup/dailychallenges/fragments/ChallengeFragment.kt
+++ app/src/main/java/com/firstventuresgroup/dailychallenges/fragments/ChallengeFragment.kt
@@ -1,17 +1,41 @@
 package com.firstventuresgroup.dailychallenges.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.firstventuresgroup.dailychallenges.R
+import com.firstventuresgroup.dailychallenges.viewmodels.ChallengeViewModel
 
 class ChallengeFragment : Fragment() {
+    private lateinit var challengeViewModel: ChallengeViewModel
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_challenge, container, false)
+        challengeViewModel = ViewModelProvider(this).get(ChallengeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_challenge, container, false)
+
+        val title: TextView = root.findViewById(R.id.challengeTitle)
+        val category: TextView = root.findViewById(R.id.challengeCategory)
+
+        val titleObserver = Observer<String> { newTitle ->
+            title.text = newTitle
+        }
+
+        val categoryObserver = Observer<String> { newCategory ->
+            category.text = newCategory
+        }
+
+        challengeViewModel.title.observe(viewLifecycleOwner, titleObserver)
+        challengeViewModel.category.observe(viewLifecycleOwner, categoryObserver)
+
+        return root
     }
 }
