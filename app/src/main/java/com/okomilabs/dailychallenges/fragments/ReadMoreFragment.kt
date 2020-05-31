cab4d4bd46ec59aba80cb451cf102ac78a1f04a3
snguyen98 <snguyen.cs@outlook.com>
2020-05-31 15:04:48 +0100

    Added navigation back to challenge and removed view model

--- app/src/main/java/com/okomilabs/dailychallenges/fragments/ReadMoreFragment.kt
+++ app/src/main/java/com/okomilabs/dailychallenges/fragments/ReadMoreFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.okomilabs.dailychallenges.R
 import com.okomilabs.dailychallenges.viewmodels.ChallengeViewModel
 
@@ -19,31 +19,25 @@ class ReadMoreFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        challengeViewModel = ViewModelProvider(this).get(ChallengeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_read_more, container, false)
+
+        val challenge: Array<String>? = arguments?.getStringArray("challenge")
 
         val title: TextView = root.findViewById(R.id.challenge_title)
         val category: TextView = root.findViewById(R.id.challenge_category)
         val summary: TextView = root.findViewById(R.id.challenge_summary)
         val desc: TextView = root.findViewById(R.id.challenge_desc)
 
-        val titleObserver = Observer<String> { newTitle ->
-            title.text = newTitle
-        }
-        val categoryObserver = Observer<String> { newCategory ->
-            category.text = newCategory
-        }
-        val summaryObserver = Observer<String> { newSummary ->
-            summary.text = newSummary
-        }
-        val descObserver = Observer<String> { newDesc ->
-            desc.text = newDesc
-        }
+        title.text = challenge?.get(0) ?: ""
+        category.text = challenge?.get(1) ?: ""
+        summary.text = challenge?.get(2) ?: ""
+        desc.text = challenge?.get(3) ?: ""
 
-        challengeViewModel.title.observe(viewLifecycleOwner, titleObserver)
-        challengeViewModel.category.observe(viewLifecycleOwner, categoryObserver)
-        challengeViewModel.summary.observe(viewLifecycleOwner, summaryObserver)
-        challengeViewModel.desc.observe(viewLifecycleOwner, descObserver)
+        // TEMPORARY NAVIGATION
+        val image: ImageView = root.findViewById(R.id.category_image)
+        image.setOnClickListener {
+            image.findNavController().navigate(ReadMoreFragmentDirections.readMoreToChallenge())
+        }
 
         return root
     }
