abf1f218514f480391fe052da2e9810ea4b43926
Sang Nguyen <snguyen.cs@outlook.com>
2020-05-31 16:01:37 +0100

    Removed unused import

--- app/src/main/java/com/okomilabs/dailychallenges/fragments/ReadMoreFragment.kt
+++ app/src/main/java/com/okomilabs/dailychallenges/fragments/ReadMoreFragment.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.okomilabs.dailychallenges.R
-import com.okomilabs.dailychallenges.viewmodels.ChallengeViewModel
 
 class ReadMoreFragment : Fragment() {
     
