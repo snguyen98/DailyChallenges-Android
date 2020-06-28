d643e6f7feab23b63ac4bb220873243c6c2ddf2d
Ned <nedeeshaw@virginmedia.com>
2020-06-28 15:34:53 +0100

    Adds drop down menu to action bar

--- app/src/main/java/com/okomilabs/dailychallenges/activities/MainActivity.kt
+++ app/src/main/java/com/okomilabs/dailychallenges/activities/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.okomilabs.dailychallenges.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.okomilabs.dailychallenges.R
 
@@ -13,6 +15,11 @@ class MainActivity: AppCompatActivity() {
 //Back button function
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
+        return true
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.drop_menu, menu)
         return true
     }
 }
