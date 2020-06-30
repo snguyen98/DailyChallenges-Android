34ab076389ac6c099f30d964d1bb248f2b02c4dc
snguyen98 <snguyen.cs@outlook.com>
2020-06-30 11:54:46 +0100

    Moved reference date into DateHelper and added documentation

--- app/src/main/java/com/okomilabs/dailychallenges/helpers/DateHelper.kt
+++ app/src/main/java/com/okomilabs/dailychallenges/helpers/DateHelper.kt
@@ -4,25 +4,44 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-class DateHelper(refDate: String) {
+/**
+ * A class to convert between dates and ints for easy database storage
+ */
+class DateHelper {
     private val format: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+    private val refDate: String = "01/06/2020"
     private val reference: Date = format.parse(refDate) ?: Date()
 
+    /**
+     * Converts a date to an integer by counting the days from the reference
+     *
+     * @param date The date to be converted into an integer
+     * @return The number of days from the reference date as an integer
+     */
+    fun dateToInt(date: String): Int {
+        val formattedDate: Date = format.parse(date) ?: return -1
+
+        return TimeUnit.DAYS.convert(
+            formattedDate.time - reference.time,
+            TimeUnit.MILLISECONDS
+        ).toInt()
+    }
+
+    /**
+     * Converts an integer to a date by using it to calculate the date by days from the reference
+     *
+     * @param days The number of days to be added to the reference
+     * @return The date as a triple in the form (dd,MM,yyyy)
+     */
     fun intToDate(days: Int): Triple<Int,Int,Int> {
         val calendar: Calendar = Calendar.getInstance()
         calendar.time = reference
         calendar.add(Calendar.DATE, days)
+
         return Triple(
             calendar.get(Calendar.DAY_OF_MONTH),
             calendar.get(Calendar.MONTH) + 1,
             calendar.get(Calendar.YEAR)
         )
-    }
-
-    fun dateToInt(date: Date): Int {
-        return TimeUnit.DAYS.convert(
-            date.time - reference.time,
-            TimeUnit.MILLISECONDS
-        ).toInt()
     }
 }
