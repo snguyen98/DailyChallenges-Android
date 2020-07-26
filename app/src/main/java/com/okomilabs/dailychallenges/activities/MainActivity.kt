21d7e29ec49482f1d6fab578e15e2f6178787d0e
snguyen98 <snguyen.cs@outlook.com>
2020-07-26 15:23:09 +0100

    Added navigation drawer to activity

--- app/src/main/java/com/okomilabs/dailychallenges/activities/MainActivity.kt
+++ app/src/main/java/com/okomilabs/dailychallenges/activities/MainActivity.kt
@@ -2,12 +2,19 @@ package com.okomilabs.dailychallenges.activities
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.material.navigation.NavigationView
 import com.okomilabs.dailychallenges.R
 
 class MainActivity: AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,26 @@ class MainActivity: AppCompatActivity() {
                 .build()
         )
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        val drawer: DrawerLayout = findViewById(R.id.drawer)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.challenge_fragment,
+                R.id.help_fragment,
+                R.id.about_fragment,
+                R.id.credits_fragment
+            ),
+            drawer
+        )
+
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment)) ||
+                super.onOptionsItemSelected(item)
     }
 
     /**
