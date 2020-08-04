7b06fdcaaa785afbe91cebcc0d5b6afb81591136
snguyen98 <snguyen.cs@outlook.com>
2020-08-04 18:21:39 +0100

    Reorganised code

--- app/src/main/java/com/okomilabs/dailychallenges/fragments/ReadMoreFragment.kt
+++ app/src/main/java/com/okomilabs/dailychallenges/fragments/ReadMoreFragment.kt
@@ -7,6 +7,7 @@ import android.transition.Slide
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -26,9 +27,23 @@ class ReadMoreFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        readMoreViewModel = ViewModelProvider(this).get(ReadMoreViewModel::class.java)
+
+        // Simple slide enter transition
+        enterTransition = Slide()
+
         val root = inflater.inflate(R.layout.fragment_read_more, container, false)
 
-        readMoreViewModel = ViewModelProvider(this).get(ReadMoreViewModel::class.java)
+        setReadMoreLayout(
+            root.findViewById(R.id.challenge_title),
+            root.findViewById(R.id.challenge_category),
+            root.findViewById(R.id.challenge_summary),
+            root.findViewById(R.id.challenge_desc),
+            root.findViewById(R.id.read_more_pointer),
+            root.findViewById(R.id.read_more_detail),
+            root.findViewById(R.id.category_icon)
+        )
+
         /* Shared element transition
         val callback: SharedElementCallback = object: SharedElementCallback() {
             override fun onSharedElementEnd(
@@ -43,22 +58,8 @@ class ReadMoreFragment: Fragment() {
         sharedElementEnterTransition = TransitionInflater.from(context)
             .inflateTransition(R.transition.read_more_transition)
 
-        setEnterSharedElementCallback(callback)
-         */
+        setEnterSharedElementCallback(callback) */
 
-        // Simple slide enter transition
-        enterTransition = Slide()
-
-
-        setReadMoreLayout(
-            root.findViewById(R.id.challenge_title),
-            root.findViewById(R.id.challenge_category),
-            root.findViewById(R.id.challenge_summary),
-            root.findViewById(R.id.challenge_desc),
-            root.findViewById(R.id.read_more_pointer),
-            root.findViewById(R.id.read_more_detail)
-        )
-      
         return root
     }
 
@@ -77,7 +78,8 @@ class ReadMoreFragment: Fragment() {
         summary: TextView,
         desc: TextView,
         pointer: LinearLayout,
-        detail: LinearLayout
+        detail: LinearLayout,
+        icon: ImageView
     ) {
         val challengeObserver = Observer<Challenge> { newChallenge ->
             title.text = newChallenge.title
@@ -88,15 +90,27 @@ class ReadMoreFragment: Fragment() {
                 desc.text = newChallenge.desc
                 showDetail(pointer, detail)
             }
-
             else {
                 pointer.visibility = View.GONE
                 detail.visibility = View.GONE
             }
 
+            showCategoryIcon(icon, newChallenge.category)
         }
 
         readMoreViewModel.challenge.observe(viewLifecycleOwner, challengeObserver)
+    }
+
+
+
+    private fun showCategoryIcon(icon: ImageView, category: String) {
+        when (category) {
+            "Physical Wellbeing" -> icon.setImageResource(R.mipmap.physical_wellbeing)
+            "Mental Wellbeing" -> icon.setImageResource(R.mipmap.mental_wellbeing)
+            "Socialising" -> icon.setImageResource(R.mipmap.socialising)
+            "Education and Learning" -> icon.setImageResource(R.mipmap.education_learning)
+            "Skills and Hobbies" -> icon.setImageResource(R.mipmap.skills_hobbies)
+        }
     }
 
     private fun showDetail(pointer: LinearLayout, detail: LinearLayout) {
